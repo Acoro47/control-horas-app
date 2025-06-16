3670715f547f354bb352731f6546d5d264ebb43b
acoro47 <abelcontry@gmail.com>
2025-06-16 14:51:26 +0200

    Creation project

---
+++ src/main/java/com/abel/control_horas_app/control_horas_app/ControlHorasAppApplication.java
@@ -0,0 +1,13 @@
+package com.abel.control_horas_app.control_horas_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlHorasAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlHorasAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abel/control_horas_app/control_horas_app/ControlHorasAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abel.control_horas_app.control_horas_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlHorasAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
