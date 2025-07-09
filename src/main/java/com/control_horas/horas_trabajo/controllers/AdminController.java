19e5a231b0ee5f4db84d3bedce1da3ebc5913bec
Abel Contreras Romance <e.acontrerasr@ies-sabadell.cat>
2025-07-09 17:59:56 +0200

    Panel de control de administración

---
+++ src/main/java/com/control_horas/horas_trabajo/controllers/AdminController.java
@@ -0,0 +1,40 @@
+package com.control_horas.horas_trabajo.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.control_horas.horas_trabajo.entities.Usuario;
+import com.control_horas.horas_trabajo.services.UsuarioDetailsService;
+
+@RestController
+@RequestMapping("/admin")
+@PreAuthorize("hasRole('ADMIN')")
+public class AdminController {
+	
+	@Autowired
+	public UsuarioDetailsService userService;
+	
+	@GetMapping("/admin")
+	public String verTodos(Model model){
+		List<Usuario> usuarios = userService.obtenerTodosUsuarios();
+		model.addAttribute("usuarios",usuarios);
+		return "admin/usuarios";
+	}
+
+	@PostMapping("/reset-password/{id}")
+	public String resetearPassword(@PathVariable Long id, RedirectAttributes redirect){
+		userService.resetearPassword(id);
+		redirect.addFlashAttribute("mensaje", "Contraseña reiniciada");
+		return "redirect:/admin/usuarios";
+	}
+}
--- src/main/java/com/control_horas/horas_trabajo/services/UsuarioDetailsService.java
+++ src/main/java/com/control_horas/horas_trabajo/services/UsuarioDetailsService.java
@@ -1,12 +1,15 @@
 package com.control_horas.horas_trabajo.services;
 
+import java.security.SecureRandom;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.control_horas.horas_trabajo.entities.Usuario;
@@ -31,6 +34,32 @@ public class UsuarioDetailsService implements UserDetailsService {
 				List.of(new SimpleGrantedAuthority("ROLE_" + user.getRol())));
 		
 		
+	}
+	
+	public List<Usuario> obtenerTodosUsuarios(){
+		return repo.findAll();
+	}
+	
+	public void resetearPassword(Long id) {
+		Usuario user = repo.findById(id).orElseThrow();
+		String claveTemporal = generarClaveTemporal();
+		String claveHash = new BCryptPasswordEncoder().encode(claveTemporal);
+		user.setPassword(claveHash);
+		repo.save(user);
+	}
+	
+	public String generarClaveTemporal() {
+		int longitud = 10;
+		String caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#%";
+		StringBuilder clave = new StringBuilder();
+		SecureRandom random = new SecureRandom();
+		
+		for (int i = 0; i < longitud; i ++) {
+			int index = random.nextInt(caracteres.length());
+			clave.append(caracteres.charAt(index));
+		}
+		
+		return clave.toString();
 	}
 
 }
