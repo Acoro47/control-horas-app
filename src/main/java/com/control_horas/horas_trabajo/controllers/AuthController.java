a0e818c492141c427f127f64d0ce51d0da182154
Abel Contreras Romance <e.acontrerasr@ies-sabadell.cat>
2025-07-09 11:25:38 +0200

    controlando los errores de acceso de usuario

--- src/main/java/com/control_horas/horas_trabajo/controllers/AuthController.java
+++ src/main/java/com/control_horas/horas_trabajo/controllers/AuthController.java
@@ -50,8 +50,10 @@ public class AuthController {
 	}
 	
 	@GetMapping("/login")
-	public String login() {
-		
+	public String login(@RequestParam(value = "error", required=false) String error, Model model) {
+		if (error != null) {
+			model.addAttribute("loginError", true);
+		}
 		return "login";
 	}
 
