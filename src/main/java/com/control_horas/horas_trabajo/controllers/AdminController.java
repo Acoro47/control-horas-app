4c87ff3d2dc2ccba4ef1e2b2301d6f3ca8eef436
Abel Contreras Romance <e.acontrerasr@ies-sabadell.cat>
2025-07-10 08:10:46 +0200

    AdminController

--- src/main/java/com/control_horas/horas_trabajo/controllers/AdminController.java
+++ src/main/java/com/control_horas/horas_trabajo/controllers/AdminController.java
@@ -3,20 +3,19 @@ package com.control_horas.horas_trabajo.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.control_horas.horas_trabajo.entities.Usuario;
 import com.control_horas.horas_trabajo.services.UsuarioDetailsService;
 
-@RestController
+@Controller
 @RequestMapping("/admin")
 @PreAuthorize("hasRole('ADMIN')")
 public class AdminController {
