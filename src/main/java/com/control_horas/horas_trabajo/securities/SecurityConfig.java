a86f2ed6a73bb84330f4dacd301b422b94dc50b8
Abel Contreras Romance <e.acontrerasr@ies-sabadell.cat>
2025-07-10 10:50:22 +0200

    seguimos depurando el authentication config

--- src/main/java/com/control_horas/horas_trabajo/securities/SecurityConfig.java
+++ src/main/java/com/control_horas/horas_trabajo/securities/SecurityConfig.java
@@ -1,23 +1,19 @@
 package com.control_horas.horas_trabajo.securities;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import com.control_horas.horas_trabajo.services.UsuarioDetailsService;
 
 @Configuration
 public class SecurityConfig {
 	
-	@Autowired
-	private UsuarioDetailsService userService;
 	
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
