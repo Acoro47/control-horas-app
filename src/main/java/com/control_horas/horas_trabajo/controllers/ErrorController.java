ed0a4c37cca422f0c16288d58008079b0b205c2c
Abel Contreras Romance <e.acontrerasr@ies-sabadell.cat>
2025-07-10 09:20:59 +0200

    depurando la vista de errores

--- src/main/java/com/control_horas/horas_trabajo/controllers/ErrorController.java
+++ src/main/java/com/control_horas/horas_trabajo/controllers/ErrorController.java
@@ -13,6 +13,14 @@ public class ErrorController implements org.springframework.boot.web.servlet.err
 	@RequestMapping("/error")
 	public String manejarError(HttpServletRequest request, Model model) {
 		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+		Object exception = request.getAttribute(RequestDispatcher.ERROR_EXCEPTION);
+		
+		String detalleError = null;
+		
+		if (exception instanceof Exception ex) {
+			detalleError = ex.getMessage();
+			
+		}
 		
 		if (status != null) {
 			int codigo = Integer.parseInt(status.toString());
@@ -31,6 +39,7 @@ public class ErrorController implements org.springframework.boot.web.servlet.err
 				
 			}
 		}
+		model.addAttribute("detalle",detalleError);
 		
 		return "error";
 	}
