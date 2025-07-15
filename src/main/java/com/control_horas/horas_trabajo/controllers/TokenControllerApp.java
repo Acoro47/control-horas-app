e4c5cbc25be7189ba7280574d70bea42851ac901
Abel Contreras Romance <e.acontrerasr@ies-sabadell.cat>
2025-07-15 12:35:50 +0200

    modificando el responseType

--- src/main/java/com/control_horas/horas_trabajo/controllers/TokenControllerApp.java
+++ src/main/java/com/control_horas/horas_trabajo/controllers/TokenControllerApp.java
@@ -1,13 +1,11 @@
 package com.control_horas.horas_trabajo.controllers;
 
-import java.util.HashMap;
-import java.util.Map;
 
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,16 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class TokenControllerApp {
 	
 	@GetMapping(value = "/enviarToken", produces = MediaType.TEXT_PLAIN_VALUE)
-	public ResponseEntity<Map<String,String>> enviarToken(
+	public @ResponseBody String enviarToken(
 			@RequestParam String username,
 			@RequestParam String email
 			){
 		
-		Map<String, String> respuesta = new HashMap<>();
-		respuesta.put("estado", "ok");
-		respuesta.put("mensaje", "✅ Token enviado correctamente");
-		
-		return ResponseEntity.ok(respuesta);
+		return "✅ Token enviado correctamente";
 		
 	}
 
