f8ccf46f81836887a9cf3f6298fc23ad9ba70fc1
Abel Contreras Romance <e.acontrerasr@ies-sabadell.cat>
2025-07-15 12:51:24 +0200

    seguimos con la api para que devuelva una cadena

--- src/main/java/com/control_horas/horas_trabajo/controllers/TokenControllerApp.java
+++ src/main/java/com/control_horas/horas_trabajo/controllers/TokenControllerApp.java
@@ -1,7 +1,10 @@
 package com.control_horas.horas_trabajo.controllers;
 
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,13 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class TokenControllerApp {
 	
-	@GetMapping(value = "/enviarToken", produces = MediaType.TEXT_PLAIN_VALUE)
-	public @ResponseBody String enviarToken(
+	@GetMapping("/enviarToken")
+	public ResponseEntity<String> enviarToken(
 			@RequestParam String username,
 			@RequestParam String email
 			){
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.TEXT_PLAIN);
 		
-		return "✅ Token enviado correctamente";
+		return new ResponseEntity<>("✅ Token enviado correctamente a " + email, headers, HttpStatus.OK);
 		
 	}
 
