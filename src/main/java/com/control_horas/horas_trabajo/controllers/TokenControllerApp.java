ff0982848d583b935c679d6e12d262f0b559d586
Abel Contreras Romance <e.acontrerasr@ies-sabadell.cat>
2025-07-15 13:04:41 +0200

    Creando el DTO de respuesta de la api

--- src/main/java/com/control_horas/horas_trabajo/controllers/TokenControllerApp.java
+++ src/main/java/com/control_horas/horas_trabajo/controllers/TokenControllerApp.java
@@ -1,29 +1,26 @@
 package com.control_horas.horas_trabajo.controllers;
 
 
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.control_horas.horas_trabajo.dtos.ApiResponseDTO;
 
 @RestController
 @RequestMapping("/api")
 public class TokenControllerApp {
 	
 	@GetMapping("/enviarToken")
-	public ResponseEntity<String> enviarToken(
+	public ResponseEntity<ApiResponseDTO> enviarToken(
 			@RequestParam String username,
 			@RequestParam String email
 			){
-		HttpHeaders headers = new HttpHeaders();
-		headers.setContentType(MediaType.TEXT_PLAIN);
 		
-		return new ResponseEntity<>("✅ Token enviado correctamente a " + email, headers, HttpStatus.OK);
+		ApiResponseDTO response = new ApiResponseDTO("success", "✅ Token enviado correctamente a \" + email)");
+		return ResponseEntity.ok(response);
 		
 	}
 
---
+++ src/main/java/com/control_horas/horas_trabajo/dtos/ApiResponseDTO.java
@@ -0,0 +1,31 @@
+package com.control_horas.horas_trabajo.dtos;
+
+public class ApiResponseDTO {
+	
+	private String  status;
+	private String message;
+	
+	public ApiResponseDTO(String status, String mes) {
+		this.status = status;
+		this.message = mes;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+
+}
