f67386ca1794309d98f794551c5baf8a1b68d9b0
Abel Contreras Romance <e.acontrerasr@ies-sabadell.cat>
2025-07-17 13:11:24 +0200

    añadiendo getters y setters

--- src/main/java/com/control_horas/horas_trabajo/dtos/app/UsuarioDTO.java
+++ src/main/java/com/control_horas/horas_trabajo/dtos/app/UsuarioDTO.java
@@ -11,5 +11,31 @@ public class UsuarioDTO {
 		this.username = name;
 		this.mensaje = message;
 	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getMensaje() {
+		return mensaje;
+	}
+
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
+	
+	
 	
 }
