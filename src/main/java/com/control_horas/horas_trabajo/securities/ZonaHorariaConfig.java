6dbd8762ff538f8f1141005a4f7a46e8a9eb5b69
Abel Contreras Romance <e.acontrerasr@ies-sabadell.cat>
2025-07-18 11:23:57 +0200

    ajustando los horarios

---
+++ src/main/java/com/control_horas/horas_trabajo/securities/ZonaHorariaConfig.java
@@ -0,0 +1,17 @@
+package com.control_horas.horas_trabajo.securities;
+
+import java.util.TimeZone;
+
+import org.springframework.context.annotation.Configuration;
+
+import jakarta.annotation.PostConstruct;
+
+@Configuration
+public class ZonaHorariaConfig {
+	
+	@PostConstruct
+	public void setZonaHorariaPorDefecto() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Europe/Madrid"));
+	}
+
+}
