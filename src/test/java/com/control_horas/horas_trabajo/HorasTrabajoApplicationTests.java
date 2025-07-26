5830fb777ebeac599951f8c95448104089221bd3
Abel Contreras Romance <e.acontrerasr@ies-sabadell.cat>
2025-07-26 16:13:15 +0200

    creado el dockerfile para arrancar el proyecto

--- src/test/java/com/control_horas/horas_trabajo/HorasTrabajoApplicationTests.java
+++ src/test/java/com/control_horas/horas_trabajo/HorasTrabajoApplicationTests.java
@@ -1,13 +1,10 @@
 package com.control_horas.horas_trabajo;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HorasTrabajoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	
 
 }
