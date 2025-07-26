4b6ea12787aabe7277ec4c26df80fbff6784d7e0
Abel Contreras Romance <e.acontrerasr@ies-sabadell.cat>
2025-07-26 11:45:24 +0200

    modificando el nuevo login por token

--- src/main/java/com/control_horas/horas_trabajo/controllers/app/LoginAppController.java
+++ src/main/java/com/control_horas/horas_trabajo/controllers/app/LoginAppController.java
@@ -2,13 +2,14 @@ package com.control_horas.horas_trabajo.controllers.app;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.control_horas.horas_trabajo.dtos.app.UsuarioDTO;
-import com.control_horas.horas_trabajo.entities.Usuario;
+import com.control_horas.horas_trabajo.dtos.app.TokenResponse;
+import com.control_horas.horas_trabajo.services.JwtService;
 import com.control_horas.horas_trabajo.services.UsuarioDetailsService;
 
 @RestController
@@ -16,29 +17,28 @@ import com.control_horas.horas_trabajo.services.UsuarioDetailsService;
 public class LoginAppController {
 	
 	private final UsuarioDetailsService userService;
+	private final JwtService jwtService;
 	
-	public LoginAppController(UsuarioDetailsService uServ) {
+	public LoginAppController(UsuarioDetailsService uServ, JwtService jServ) {
 		this.userService = uServ;
+		this.jwtService = jServ;
 	}
 	
 	@PostMapping("/login")
-	public ResponseEntity<UsuarioDTO> login(
+	public ResponseEntity<TokenResponse> login(
 			@RequestParam String username,
 			@RequestParam String password
 			) {
 		boolean valido = userService.validarCredenciales(username, password);
 		if (valido) {
+			UserDetails userDetails = userService.loadUserByUsername(username);
+			String token = jwtService.generateToken(userDetails);
 			
-			Usuario user = userService.obtenerUsuarioPorNombre(username);
-			UsuarioDTO userDto = new UsuarioDTO(
-					user.getId(), user.getUsername(), "✅ Inicio de sesión correcto"
-					);
-			
-			return ResponseEntity.ok(userDto);
+			return ResponseEntity.ok(new TokenResponse(token));
 		}
 		else {
 			return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-					.body(null);
+					.build();
 		}
 	}
 
---
+++ src/main/java/com/control_horas/horas_trabajo/dtos/app/TokenResponse.java
@@ -0,0 +1,5 @@
+package com.control_horas.horas_trabajo.dtos.app;
+
+public record TokenResponse(String token) {
+
+}
