98a52c44770da6e597a6e6a4719658ae46d1823d
Abel Contreras Romance <e.acontrerasr@ies-sabadell.cat>
2025-07-28 18:55:46 +0200

    alineado del backend y la app

--- src/main/java/com/control_horas/horas_trabajo/dtos/app/TokenResponse.java
+++ src/main/java/com/control_horas/horas_trabajo/dtos/app/TokenResponse.java
@@ -1,5 +1,3 @@
 package com.control_horas.horas_trabajo.dtos.app;
 
-public record TokenResponse(Long id, String username, String mensaje,String token) {
-
-}
+public record TokenResponse(Long id, String username, String mensaje,String token) {}
