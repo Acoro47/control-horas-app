29ca84cfe8578217ac00032edf17fa46a6bd6c51
Abel Contreras Romance <e.acontrerasr@ies-sabadell.cat>
2025-07-31 11:33:56 +0200

    probando el login de la app para crear el pdf

--- src/main/java/com/control_horas/horas_trabajo/securities/SecurityConfig.java
+++ src/main/java/com/control_horas/horas_trabajo/securities/SecurityConfig.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.control_horas.horas_trabajo.components.CustomSuccessHandler;
@@ -44,7 +46,10 @@ public class SecurityConfig {
 		.securityMatcher("/api/**")
 		.csrf(csrf -> csrf.disable()) 
 		.sessionManagement(session -> session
-				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) 
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+		.exceptionHandling(ex -> ex
+				.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+				)
 		.authorizeHttpRequests(auth -> auth 
 				.requestMatchers("/api/login", "/api/enviarToken").permitAll() 
 				.anyRequest().authenticated() 
