1f33fb8f08954b8cd620447569410756b35fdf02
Abel Contreras Romance <e.acontrerasr@ies-sabadell.cat>
2025-09-22 15:26:01 +0200

    mejorando el endpoint para comprobar el estado del servidor

--- src/main/java/com/control_horas/horas_trabajo/controllers/app/WakeUpController.java
+++ src/main/java/com/control_horas/horas_trabajo/controllers/app/WakeUpController.java
@@ -9,25 +9,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.control_horas.horas_trabajo.services.ServerStatusService;
 
 @RestController
 @RequestMapping("/api")
 public class WakeUpController {
-	
-	private final ServerStatusService serverStatus;
-	
-	public WakeUpController(ServerStatusService service) {
-		this.serverStatus = service;
-	}
-	
+		
 	@GetMapping("/wakeup")
 	public ResponseEntity<Map<String, String>> wakeup(){
 		
 		Map<String, String> response = new HashMap<>();
-		boolean serverRunning = serverStatus.checkServerStatus();
-		
-		response.put("status", serverRunning ? "running" : "stopped");
+				
+		response.put("status", "ok");
 		response.put("timestamp", Instant.now().toString());
 		return ResponseEntity.ok(response);
 		
