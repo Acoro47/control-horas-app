4f6ddad337b22bd6bbd84dca12f49fbcbb601942
Abel Contreras Romance <e.acontrerasr@ies-sabadell.cat>
2025-11-06 21:20:48 +0100

    volviendo al acceso funcional

--- src/main/java/com/control_horas/horas_trabajo/controllers/web/AuthController.java
+++ src/main/java/com/control_horas/horas_trabajo/controllers/web/AuthController.java
@@ -20,12 +20,15 @@ public class AuthController {
 		
 	
 	@GetMapping("/login")
-	public String login(@RequestParam(value = "error", required=false) String error,
-			Model model) {
-		if (error != null) {
-			model.addAttribute("loginError", true);
-		}
-		
-		return "login";
+	public String login(@RequestParam(value = "error", required = false) String error,
+	                    @RequestParam(value = "logout", required = false) String logout,
+	                    Model model) {
+	    if (error != null) {
+	        model.addAttribute("error", "Credenciales incorrectas. Intenta de nuevo.");
+	    }
+	    if (logout != null) {
+	        model.addAttribute("mensaje", "Has cerrado sesión correctamente.");
+	    }
+	    return "login";
 	}
 }
