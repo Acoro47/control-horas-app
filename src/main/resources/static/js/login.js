3b677a05b97f0c98e2a95ed6efe19953743fa569
Abel Contreras Romance <e.acontrerasr@ies-sabadell.cat>
2025-11-06 17:03:49 +0100

    mas pruebas del login

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -3,8 +3,23 @@ console.log("login.js cargado");
 
 function desactivar_boton(){
 	
+	const form = document.querySelector('#loginForm');	
 	const btnLogin = document.querySelector('#loginBtn');
-	console.log(btnLogin);
+	const btnLoading = document.querySelector('#loginBtnDisabled');
+	const overlay = document.querySelector('#submitOverlay');
+	const message = document.querySelector('#message');
+	const error = document.querySelector('#error')
+	
+	form.addEventListener('submit', (e) => {
+		if (btnLogin.classList.contains('d-none')) {
+			e.preventDefault();
+			return;
+		}
+	});
+	
+	console.log(message);
+	console.log(error);
+	
 	
 }
 
