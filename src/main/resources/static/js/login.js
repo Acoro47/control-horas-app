a62b318c6ba6d97e3167607b424fc81502c85365
Abel Contreras Romance <e.acontrerasr@ies-sabadell.cat>
2025-11-06 20:58:36 +0100

    probando el error desde el backend

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -4,17 +4,13 @@ console.log("login.js cargado");
 function desactivar_boton(){
 	
 	const form = document.querySelector('#loginForm');	
-	const btnLogin = document.querySelector('#loginBtn');
-	
-	
-		
 	
 	form.addEventListener('submit', (e) => {
 		e.preventDefault();
-		console.log("Enviando el formulario...");
-		console.log("BtnLogin", btnLogin);
-		btnLogin.ariaHidden = true;
-		console.log("BtnLogin", btnLogin);
+		const error = document.querySelector('#error');
+		if (error != null) {
+			console.log("Error: ", error);
+		}
 		
 	});
 	
